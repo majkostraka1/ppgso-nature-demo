626ffbc6415bba1743f357b211dc54d0f50ecea0
Marek Straka <43640827+majkostraka1@users.noreply.github.com>
2021-11-25 15:54:15 +0100

    Add files via upload

--- shader/diffuse_frag.glsl
+++ shader/diffuse_frag.glsl
@@ -5,6 +5,12 @@ uniform sampler2D Texture;
 // Direction of light
 uniform vec3 LightDirection;
 
+// Lightning color
+uniform vec3 LightColor;
+
+// Position of light
+uniform vec3 LightPosition;
+
 // (optional) Transparency
 uniform float Transparency;
 
@@ -21,11 +27,27 @@ in vec4 normal;
 out vec4 FragmentColor;
 
 void main() {
+  // Compute ambient lightning
+  float ambientStrength = 0.25f;
+  vec3 ambient = ambientStrength * LightColor;
+
+  // Compute specular lightning
+  float specularStrength = 0.5;
+  vec3 viewDir = normalize(LightPosition - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);
+
   // Compute diffuse lighting
   float diffuse = max(dot(normal, vec4(normalize(LightDirection), 1.0f)), 0.0f);
 
+  vec3 objectColor = texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y)).xyz;
+
+  vec3 result = (ambient + diffuse) * objectColor;
+
+  FragmentColor = vec4(result, 1.0f);
+
   // Lookup the color in Texture on coordinates given by texCoord
   // NOTE: Texture coordinate is inverted vertically for compatibility with OBJ
-  FragmentColor = texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y) + TextureOffset) * diffuse;
+  //FragmentColor = texture(Texture, vec4(result, 1.0) + TextureOffset);
+  //vec2(texCoord.x, 1.0 - texCoord.y)
   FragmentColor.a = Transparency;
 }
